49595ed6543f32d3dea1e7632ad1db01a15c6922
Aladin <sabbaghalaeddine@live.fr>
2024-09-17 22:48:11 +0200

    feat: Add Clerk middleware for protected routes

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher(["/"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, redirectToSignIn } = auth();
+
+  // If the user isn't signed in and the route is private, redirect to sign-in
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn({ returnBackUrl: "/" });
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+};
